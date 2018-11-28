3eb107b4af7524f11ca9c83d656e6dbe0397197d
Michael Kevin Lo <lom@eeapps01.seas.ucla.edu>
2018-11-28 01:20:52 -0800

    784 input 1 neuron

--- Verilog/test_tb.v
+++ Verilog/test_tb.v
@@ -26,22 +26,22 @@ initial begin
 	#fullclock GlobalReset = 1'b0;
 
 	for(i = 0; i < 784; i = i+1) begin
-		A[i*19 +: 19] = 19'b010_0000_0000_0000_0000;
- 		B[i*10 +: 10] = i%4;
+		A[i*19 +: 19] = 19'b000_1000_0000_0000_0000;
+ 		B[i*10 +: 10] = i%2;
 	end
 
-	for(i = 0; i < 800; i = i + 1)begin
+	for(i = 0; i < 810; i = i + 1)begin
 		#fullclock;
 	end
 
-	$display("%b%b%b%b%b%b%b%b.%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b",
+	$display("%b%b%b%b%b%b%b%b.%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b%b %d",
 		result[25], result[24], result[23], result[22],
 		result[21], result[20], result[19], result[18],
 		result[17], result[16], result[15], result[14],
 		result[13], result[12], result[11], result[10],
 		result[9], result[8], result[7], result[6],
 		result[5], result[4], result[3], result[2], result[1],
-		result[0]);
+		result[0], result[25:18]);
 
 	$stop;
 end
