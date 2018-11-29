f3811aac8266d8a41898512d7fe67f51a4ff4efd
Dennis Shim <hisroar@gmail.com>
2018-11-28 16:55:48 -0800

    Added comment for input Weights

--- Verilog/VectorMatrixProduct.v
+++ Verilog/VectorMatrixProduct.v
@@ -1,12 +1,15 @@
 module VectorMatrixProduct
-#(parameter PIXEL_N = 10,
-  parameter WEIGHT_SIZE = 19,
-  parameter PIXEL_SIZE = 10,
-  parameter N = 10)
+#(parameter PIXEL_N = 10, // number of pixels (785)
+  parameter WEIGHT_SIZE = 19, // bit-width of weights
+  parameter PIXEL_SIZE = 10,  // bit-width of pixels
+  parameter N = 10) // Number of neurons
 (
    input clk,
    input GlobalReset,
    input [PIXEL_N*PIXEL_SIZE-1:0] Pixels,
+   // Weights input must be as follows for this to work:
+   // Weights[  WEIGHT_SIZE*PIXEL_N-1 : 0]                   = [Wgt_0_784, Wgt_0_783, ... Wgt_0_1, Wgt_0_0]
+   // Weights[2*WEIGHT_SIZE*PIXEL_N-1 : WEIGHT_SIZE*PIXEL_N] = [Wgt_1_784, Wgt_1_783, ... Wgt_1_1, Wgt_1_0]
    input [PIXEL_N*WEIGHT_SIZE*N-1:0] Weights, // rows are length PIXEL_N, cols N
    output [26*N-1:0] value
 );
